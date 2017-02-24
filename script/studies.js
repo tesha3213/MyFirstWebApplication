0668878708f7cba6702ba1c64fe811d7c52f4dfb
tesha3213 <dandelion1252@gmail.com>
2017-02-24 15:07:35 +0700

    Application using JS

--- script/studies.js
+++ script/studies.js
@@ -1,14 +1,31 @@
 window.onload = function(){
 
-	var tds = document.getElementsByTagName('td')
-	var trs = document.getElementsByTagName('tr')
+	var tds = document.getElementsByTagName('td');
+	var trs = document.getElementsByTagName('tr');
+	var myTd = document.getElementById('my-td');
 	for (var i = 0; i < tds.length; i++) {
 		console.log(tds[i].innerText);
 
 		tds[i].addEventListener('click', function(event){
 			event.preventDefault();
-			alert(this.innerText);
+			var highlighted = document.getElementsByClassName('highlighted');
+			for (var i = highlighted.length - 1; i >= 0; i--) {
+				highlighted[i].classList.remove('highlighted');
+			}
+			this.classList.add('highlighted');
 		});
+
+		tds[i].addEventListener('mouseover', function(event){
+			event.preventDefault();
+			myTd.innerHTML = '<span class="my-td-span">' + this.innerText + '</span>';
+		});
+
+		tds[i].addEventListener('mouseout', function(event){
+			event.preventDefault();
+			myTd.innerHTML = '';
+		});
+
+		
 	}
 	for (var i = trs.length - 1; i >= 0; i--) {
 		trs[i].addEventListener('click', function(event){
@@ -17,8 +34,8 @@ window.onload = function(){
 		})
 	}
 
-		trs[0].addEventListener('mouseover', function(event){
+		/*trs[0].addEventListener('mouseover', function(event){
 			event.preventDefault();
 			alert('Pluto has come !')
-	});
+	});*/
 }
